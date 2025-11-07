3c4271633c5730668cf964413c46948e9a4ce07a
oraclegods <folufolusho@gmail.com>
2025-11-07 14:52:44 +0100

    addProducttoCart updated

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,16 @@
-import { setLocalStorage } from "./utils.mjs";
+//import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const currentCart = JSON.parse(localStorage.getItem("so-cart")) || [];
+  // setLocalStorage("so-cart", product);
+   // add the new product
+  currentCart.push(product);
+
+   // save updated cart back to localStorage
+  localStorage.setItem("so-cart", JSON.stringify(currentCart));
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
