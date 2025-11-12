56ece532ca49e86165ba60b8e660061f188c4a31
oraclegods <folufolusho@gmail.com>
2025-11-12 13:35:27 +0100

    productdetails updated

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,3 +21,18 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get(param);
+}
+
+// Keep the existing setLocalStorage and getLocalStorage functions
+
+/* export function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+export function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+} */
