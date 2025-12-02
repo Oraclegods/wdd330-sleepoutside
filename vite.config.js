f259db7fcfe42cd2c2231fc028aca69b311fa2d3
oraclegods <folufolusho@gmail.com>
2025-12-02 13:54:50 +0100

    new vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src/",
+  root: "src",
+  base: "/", // <--- THIS prevents the "Flash & 404" issue!
   build: {
     outDir: "../dist",
     rollupOptions: {
@@ -10,6 +11,8 @@ export default defineConfig({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
+        // Double check: Do you have a folder named 'product_pages'? 
+        // If not, delete this line or change it to 'product' if that is the folder name.
         product: resolve(__dirname, "src/product_pages/index.html"),
         listing: resolve(__dirname, "src/product_listing/index.html"),
       },
